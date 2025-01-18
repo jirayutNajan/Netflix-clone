ab1f8e7edfdf2f25bef7ed083cfc35fafbe2b620
Jirayut Najan <tonklaeiei123@gmail.com>
2025-01-18 21:55:29 +0700

    complete build auth screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/Home/HomePage";
 import SignupPage from "./pages/SignupPage";
+import Footer from "./components/footer";
 
 const app = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
